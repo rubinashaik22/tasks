74fd297bb05720a486a66818d6c0f501a0635e08
rubina <srubina886@gmail.com>
2024-03-08 20:38:02 -0500

    solved-forms

--- src/form-components/CheckAnswer.tsx
+++ src/form-components/CheckAnswer.tsx
@@ -1,13 +1,27 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function CheckAnswer({
     expectedAnswer
 }: {
     expectedAnswer: string;
 }): JSX.Element {
+    const [answer, setAnswer] = useState<string>("");
     return (
         <div>
             <h3>Check Answer</h3>
+            <Form.Group controlId="checking answer">
+                <Form.Label>Check Answer</Form.Label>
+                <Form.Control
+                    type="string"
+                    value={answer}
+                    onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+                        setAnswer(event.target.value)
+                    }
+                />
+            </Form.Group>
+            {answer == expectedAnswer && <div>✔️</div>}
+            {answer != expectedAnswer && <div>❌</div>}
         </div>
     );
 }
